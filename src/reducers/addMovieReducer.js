cfc90d5f4d2d3dc8d1d7cf5a848de65aaf81b4b6
sangram <sangramvbarge@gmail.com>
2020-01-13 22:29:27 +0530

    need to update redux store currently fetching data to reducer js

--- src/reducers/addMovieReducer.js
+++ src/reducers/addMovieReducer.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 const initialState = [{name:"inception", price:"$25"}];
 const addMovieReducer = (state=initialState, action) => {
 
-    console.log(action.payload)
+    console.log(action.payload);
     switch(action.type) {
         case  'ADD':
-                return initialState;
+                let newMovieList = state;
+                return Object.assign(newMovieList, [{name: action.payload.name, price:action.payload.movieprice }]);
+                
         default:
                 return initialState;
     } 
